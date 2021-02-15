67ecd9ed447a64d127d83e370522d99fed0887bd
pwoicik <patrykwoicik@gmail.com>
2021-02-15 15:58:54 +0100

    run on port from env

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"chat-rooms/internal/server"
 	"chat-rooms/internal/server/websocket"
+	"os"
 )
 
 func main() {
 	go websocket.GetHub().Run()
 
-	_ = server.CreateServer().Run()
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = ":8080"
+	}
+	_ = server.CreateServer().Run(port)
 }
